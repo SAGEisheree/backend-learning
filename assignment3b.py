2d8dac4cb0a3d6350ada4bfb7038ea5693b5b623
Abhinav <abhinavakarthikeya01@gmail.com>
2026-02-21 22:05:58 +0530

    done 2nd and 3rd tasks

---
+++ assignment3b.py
@@ -0,0 +1,9 @@
+import math
+def task(number):
+    a=math.sqrt(number)
+    b=math.log(number)
+    c=math.sin(number)
+    return a,b,c
+number = int(input("Enter a number: "))
+x,y,z=task(number)
+print(f"Square root:{x}\nLogarithm:{y}\nSine:{z}")
